ca8014be78d856ceb1cf8f8c945c022330fefb4b
Aurelien <magixmushroom@gmail.com>
2014-11-03 15:08:14 +0100

    Premier Commit

---
+++ Assets/Scripts/InvadersManager.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvadersManager : MonoBehaviour
+{
+	public Transform invader1Prefab;
+	public Transform invader2Prefab;
+	public Transform invader3Prefab;
+	public Transform invader;
+	public Transform[] invaderTab;
+	public float vitesseDeplacement;
+	public int score = 0;
+	public bool defaite;
+	private GUIText victoireLabel;
+	public bool vide = true;
+	public Transform flyingSaucerPrefab;
+	public Transform saucer;
+	public bool saucerBool = false;
+	public Transform saucerBulletPrefab;
+	public Transform saucerBullet;
+	public float timerSaucer = 0f;
+	
+	private bool goingRight = true;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		victoireLabel = GameObject.Find("VictoireLabel").guiText;
+		vitesseDeplacement = 10f;
+		invaderTab = new Transform[55];
+		int cpt = 0;
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j<11; j++){
+				if (i == 0)
+				{
+					invader = Instantiate(invader3Prefab) as Transform;
+				}
+				if (i > 0 && i < 3)
+				{
+					invader = Instantiate(invader2Prefab) as Transform;
+				}
+				if (i >= 3) 
+				{
+					invader = Instantiate(invader1Prefab) as Transform;
+				}
+				invader.transform.position = new Vector3(j*3f - 5.5f*3f, i*3f - 2*3f, 0f);
+				invader.parent = transform;
+				invaderTab[cpt] = invader;
+				cpt++;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		timerSaucer += Time.deltaTime;
+		//Gestion du FlyingSaucer
+		if (!saucerBool) {
+			if (Random.value+Random.value > 1.9) {
+					saucer = Instantiate (flyingSaucerPrefab) as Transform;
+					saucer.transform.position = new Vector3 (-38f, 10f, 0f);
+					saucerBool = true;
+			}
+		} else {
+			saucer.transform.Translate(new Vector3(0.2f, 0f, 0f));	
+			shoot ();
+		}
+
+		//Deplacement des invaders si le tableau n'est pas vide
+		vide = true;
+		for (int i = 0; i < 55; i++) {
+			if (invaderTab [i] != null) {
+				vide = false;
+				if (goingRight) {
+					invaderTab [i].transform.Translate (new Vector3 (vitesseDeplacement* Time.deltaTime, 0f, 0f));
+				} else {
+					invaderTab [i].transform.Translate (new Vector3 (-vitesseDeplacement * Time.deltaTime, 0f, 0f));
+				}
+			}	
+		}
+		if (vide) {
+			victoireLabel.text = "Victoire !";
+			Time.timeScale = 0f;		
+		}
+	}
+
+	public void moveInvadersCloser ()
+	{
+		goingRight = !goingRight;
+		Debug.Log (goingRight);
+		vitesseDeplacement += 3f;
+		for (int i = 0; i < 55; i++)
+		{
+			if (invaderTab[i] != null)
+			{
+				invaderTab[i].transform.Translate(new Vector3(0f, -40f*Time.deltaTime, 0f));
+			}
+		}
+	}
+
+	private void shoot()
+	{
+		if (timerSaucer > 0.4f) {
+			saucerBullet = Instantiate(saucerBulletPrefab) as Transform;
+			if (saucerBullet != null) 
+			{
+				saucerBullet.transform.position = new Vector3(saucer.transform.position.x, saucer.transform.position.y, 0f);
+				saucerBullet.rigidbody2D.velocity = new Vector2(0f, -15f);
+			}
+			timerSaucer = 0f;
+		}
+	}
+
+	public void destroySaucer (Transform saucer)
+	{
+		Destroy (saucer.gameObject);
+		saucerBool = false;
+	}
+
+	public void destroyInvader (Transform invader)
+	{
+		score += 1;
+		for (int i = 0; i < 55; i++) {
+			if (invaderTab[i] == invader)
+			{
+				Destroy (invader.gameObject);
+				Destroy (invaderTab[i].gameObject);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour
+{
+	/// <summary>
+	/// The bullet prefab.
+	/// </summary>
+	public Transform bulletPrefab;
+	private Transform bullet;
+	private GUIText livesLabel;
+	public float timer = 0f;
+	public float vitesseTir = 0.3f;
+	public int nblives = 3;
+	private GUIText defaiteLabel;
+	public bool boolDefaite = false;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		livesLabel = GameObject.Find("LivesLabel").guiText;
+		defaiteLabel = GameObject.Find("DefaiteLabel").guiText;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (nblives <= 0) {
+			defaiteLabel.text = "Défaite !";
+			boolDefaite = true;
+		}
+		if (boolDefaite) {
+			Time.timeScale = 0;		
+		}
+		livesLabel.text = "Lives: " + nblives;
+		
+		// Left movement
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (-16 * Time.deltaTime, 0, 0);
+		}
+		// Right movement
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate (16 * Time.deltaTime, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			if (timer > vitesseTir) {
+				shoot ();
+				timer = 0f;
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.LeftControl)) {
+			vitesseTir -= 0.1f;
+		}
+		timer += Time.deltaTime;
+	}
+	
+	private void shoot()
+	{
+		bullet = Instantiate(bulletPrefab) as Transform;
+		if (bullet != null) 
+		{
+			bullet.transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
+			bullet.rigidbody2D.velocity = new Vector2(0f, 15f);
+		}
+	}
+	
+	public bool getBoolDefaite()
+	{
+		return boolDefaite;
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if (collision.collider.CompareTag("Invader"))
+		{
+			Destroy(collision.collider.gameObject);
+			nblives -= 1;
+			transform.position = new Vector3(0, transform.position.y, 0);
+		}
+	}
+}
